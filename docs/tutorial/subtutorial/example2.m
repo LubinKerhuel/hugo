90162aa0e0c851ab1cb22727e34c531c24ec867c
Lubin <lubinkerhuel@yahoo.fr>
2018-12-17 12:33:33 +0100

    Added back all files fixing problem with cases

---
+++ docs/tutorial/subtutorial/example2.m
@@ -0,0 +1,23 @@
++++
+title = "Example Page 12"
+
+date = 2018-09-09T00:00:00
+# lastmod = 2018-09-09T00:00:00
+
+draft = false  # Is this a draft? true/false
+toc = true  # Show table of contents? true/false
+type = "docs"  # Do not modify.
+
+# Add menu entry to sidebar.
+linktitle = "Example Page12"
+[menu.subtutorial]
+  parent = "Example Topic"
+  weight = 1
++++
+
+$\frac{1}{pi}$
+
+#include hardware_setup_lnk.md
+\include{"hardware_setup_lnk.md"}
+
+{{hardware_setup_lnk.md}}
