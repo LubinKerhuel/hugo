46bb73af067bfc57f46de5c3d63e6d3e8540aca1
Lubin <lubinkerhuel@yahoo.fr>
2021-02-20 11:17:44 +0100

    improve batch sripts

---
+++ Hugo_DraftList.bat
@@ -0,0 +1,2 @@
+hugo list drafts 
+pause
--- Hugo_build.bat
+++ Hugo_build.bat
@@ -1,18 +1,18 @@
-rem rmdir /Q /S docs
-rem hugo -t "academic/wowchemy" -F --i18n-warnings --minify --enableGitInfo --cleanDestinationDir  --destination /docs 
+@rem rmdir /Q /S docs
+@rem hugo -t "academic/wowchemy" -F --i18n-warnings --minify --enableGitInfo --cleanDestinationDir  --destination /docs 
 hugo version
-hugo list drafts 
+@rem hugo list drafts 
 
 hugo -t "academic/wowchemy" --environment production -F --i18n-warnings --gc --cleanDestinationDir --minify --enableGitInfo --forceSyncStatic --destination /docs 
 hugo -t "academic/wowchemy" --environment production -F --i18n-warnings --minify --enableGitInfo --destination /docs 
-rem hugo -t "academic/wowchemy" -F --i18n-warnings --gc --cleanDestinationDir --minify --enableGitInfo --forceSyncStatic --destination /docs 
+@rem hugo -t "academic/wowchemy" -F --i18n-warnings --gc --cleanDestinationDir --minify --enableGitInfo --forceSyncStatic --destination /docs 
 
 git add docs/*
 @echo off
-rem  get total number of lines from dir output
+@rem  get total number of lines from dir output
 FOR /F "delims=" %%i IN ('dir /S docs ^| find "asdfasdfasdf" /C /V') DO set lineCount=%%i
-rem  dir summary is always last 3 lines; calculate starting line of summary info
+@rem  dir summary is always last 3 lines; calculate starting line of summary info
 set /a summaryStart="lineCount-3"
-rem  now output just the last 3 lines
+@rem  now output just the last 3 lines
 dir /S docs | more +%summaryStart%
 pause
